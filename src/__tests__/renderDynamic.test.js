e0d2bf7450ec10289840d8934d6fb5529c94df65
kriswep <kriswep@wetainment.com>
2019-11-02 12:37:06 +0100

    functions on ci

--- src/__tests__/renderDynamic.test.js
+++ src/__tests__/renderDynamic.test.js
@@ -1,16 +1,16 @@
 import 'isomorphic-fetch';
 
 test('generate dynamic pdf', async () => {
-  // const res = await fetch('http://localhost:4000', {
-  //   method: 'POST',
-  //   headers: { 'Content-Type': 'application/json' },
-  //   body: JSON.stringify({
-  //     query:
-  //       '{ document(doc: [{ text: "BIG", size: BIG } { text: "normal" } { text: "small", size: SMALL }]) { blob } }',
-  //   }),
-  // });
-  // const { data } = await res.json();
-  // const pdfBlob = data.document.blob;
-  // expect(pdfBlob.startsWith('data:application/pdf;base64,')).toBeTruthy();
+  const res = await fetch('http://localhost:8888/.netlify/functions/grapdf', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      query:
+        '{ document(doc: [{ text: "BIG", size: BIG } { text: "normal" } { text: "small", size: SMALL }]) { blob } }',
+    }),
+  });
+  const { data } = await res.json();
+  const pdfBlob = data.document.blob;
+  expect(pdfBlob.startsWith('data:application/pdf;base64,')).toBeTruthy();
   expect(1).toBeTruthy();
 });
