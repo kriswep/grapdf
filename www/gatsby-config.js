d85af8c3666278b400d8b2c80b4dcf029c5434ee
kriswep <kriswep@wetainment.com>
2020-01-14 22:34:31 +0100

    integrate demo querying

--- www/gatsby-config.js
+++ www/gatsby-config.js
@@ -4,6 +4,10 @@ module.exports = {
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
+  proxy: {
+    prefix: "/.netlify",
+    url: "http://localhost:8888",
+  },
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
--- www/src/pages/page-2.js
+++ www/src/pages/page-2.js
@@ -1,16 +1,76 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = () => (
-  <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+const initialQuery = `{
+  document(
+    doc: [
+      { title: { text: "Normal Title" } }
+      { text: { text: "Normal Text" } }
+      { title: { text: "Small Title", size: SMALL } }
+      { text: { text: "Small Text", size: SMALL } }
+      { title: { text: "Big Title", size: BIG } }
+      { text: { text: "Big Text", size: BIG } }
+    ]
+  ) {
+    blob
+  }
+}`
+
+const SecondPage = () => {
+  const [query, setQuery] = useState(initialQuery)
+
+  const sendQuery = async evt => {
+    const pdfBlob = await fetch("/.netlify/functions/grapdf", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ query }),
+    })
+      .then(res => console.log(res) || res.json())
+      .then(({ data, errors }) => {
+        if (errors) {
+          throw new Error(
+            errors.reduce(
+              (messages, currentError) =>
+                messages
+                  ? `${messages};
+${currentError.message}`
+                  : currentError.message,
+              ""
+            )
+          )
+        }
+
+        return data.document.blob
+      })
+      .catch(err => {
+        alert(`Error fetching: ${err.message}`)
+      })
+    if (!pdfBlob) return
+    const link = document.createElement("a")
+    link.href = pdfBlob
+    link.download = "file.pdf"
+    link.click()
+  }
+
+  return (
+    <Layout>
+      <SEO title="Demo" />
+      <h1>Demo</h1>
+      <p>Change and send the query</p>
+      <label htmlFor="query">Enter your Query! </label>
+      <textarea
+        rows="10"
+        id="query"
+        value={query}
+        onChange={evt => setQuery(evt.target.value)}
+      />
+      <button onClick={sendQuery}>Get PDF</button>
+      <Link to="/">Go back to the homepage</Link>
+    </Layout>
+  )
+}
 
 export default SecondPage
