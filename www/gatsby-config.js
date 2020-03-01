4f483edd08e93c267c595a238e36ecccd6f54764
kriswep <kriswep@wetainment.com>
2020-03-01 13:18:51 +0100

    enable analytics

--- www/gatsby-config.js
+++ www/gatsby-config.js
@@ -32,6 +32,16 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-matomo`,
+      options: {
+        siteId: `2`,
+        matomoUrl: `https://matomo.wetainment.com`,
+        siteUrl: `https://grapdf.com`,
+        disableCookies: true,
+        // dev: false,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
